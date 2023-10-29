51d602be72a702e62ce5535c73193837f13e0fdb
emrekukula <emrekukula@gmail.com>
2023-10-29 14:15:34 +0300

    Turned constructor into class

--- scripts.js
+++ scripts.js
@@ -10,11 +10,13 @@ const subBtn = document.querySelector('.subBtn');
 
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.read = read
+class Book {
+    constructor (title, author, pages, read) {
+        this.title = title,
+        this.author = author,
+        this.pages = pages,
+        this.read = read
+    }
 }
 
 function addBookToLibrary(name, author, pages, read) {
